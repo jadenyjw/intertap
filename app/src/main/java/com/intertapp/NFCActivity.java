803ffa380c1dd4452e5d7411fc97c91fc3567d3f
Jaden Wang <jadenyjw@gmail.com>
2018-01-20 14:55:24 -0500

    Get requests to work

--- app/src/main/java/com/intertapp/NFCActivity.java
+++ app/src/main/java/com/intertapp/NFCActivity.java
@@ -9,12 +9,39 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
 
     private EditText mEditText;
+    private final String accessID = "CA1TAaPamkkrCsZQ";
+    private final String registration = "CA1AReggCJXVeGF2";
+    private final String secretKey = "XyhKlB3ySVGpeNHAWR7XwkASvpiymC/Sr4GQDyZs+YM="; //lul
+    private final String salt = "uthacks";
+    private String accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +60,7 @@ public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessag
         }
 
         mAdapter.setNdefPushMessageCallback(this, this);
+
     }
 
     /**
@@ -42,8 +70,10 @@ public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessag
      */
     @Override
     public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
-        String message = mEditText.getText().toString();
-        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
+
+        String request = "6";
+        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", request.getBytes());
+
         NdefMessage ndefMessage = new NdefMessage(ndefRecord);
         return ndefMessage;
     }
--- app/src/main/java/com/intertapp/NFCDisplayActivity.java
+++ app/src/main/java/com/intertapp/NFCDisplayActivity.java
@@ -1,23 +1,68 @@
 package com.intertapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.zip.GZIPInputStream;
 
 
 public class NFCDisplayActivity extends Activity {
 
     private TextView mTextView;
+    private String strippedPhone;
+    private final String accessID = "CA1TAaPamkkrCsZQ";
+    private final String registration = "CA1AReggCJXVeGF2";
+    private final String secretKey = "XyhKlB3ySVGpeNHAWR7XwkASvpiymC/Sr4GQDyZs+YM="; //lul
+    private final String salt = "uthacks";
+    private String accessToken;
+    private RequestQueue mRequestQueue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_display);
         mTextView = (TextView) findViewById(R.id.text_view);
+        TelephonyManager tMgr = (TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        String mPhoneNumber = tMgr.getLine1Number();
+        mTextView.setText(mPhoneNumber);
+        strippedPhone = mPhoneNumber.substring(1);
+        System.out.println(strippedPhone);
+
+
+
     }
 
     @Override
@@ -29,7 +74,110 @@ public class NFCDisplayActivity extends Activity {
                     NfcAdapter.EXTRA_NDEF_MESSAGES);
 
             NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
-            mTextView.setText(new String(message.getRecords()[0].getPayload()));
+
+            System.out.println(new String(message.getRecords()[0].getPayload()));
+            final String request = new String(message.getRecords()[0].getPayload());
+            final double amount = Double.parseDouble(request);
+
+            String url = "https://gateway-web.beta.interac.ca/publicapi/api/v1/access-tokens";
+
+            Network network = new BasicNetwork(new HurlStack());
+            Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+            mRequestQueue = new RequestQueue(cache, network);
+            mRequestQueue.start();
+
+            JSONObject obj;
+
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println(response.toString());
+                            try {
+                                accessToken = (String) response.get("access_token");
+                                final String requestID = String.valueOf(UUID.randomUUID()).substring(0,8);
+                                System.out.println(accessToken + " is the token");
+                                String json = "{ \"sourceMoneyRequestId\": \""+ requestID + "\", \"requestedFrom\": { \"contactName\": \"UofT Hacks\", \"language\": \"en\", \"notificationPreferences\": [ { \"handle\": \""+ strippedPhone + "\", \"handleType\": \"sms\", \"active\": true } ] }, \"amount\": "+amount +", \"currency\": \"CAD\", \"supressResponderNotifications\": false, \"expiryDate\": \"2018-02-30T16:12:12.721Z\", \"editableFulfillAmount\": false }";
+                                System.out.println(json);
+                                String url = "https://gateway-web.beta.interac.ca/publicapi/api/v2/money-requests/send";
+                                try {
+                                    JSONObject obj = new JSONObject(json);
+                                    JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                                            (Request.Method.POST, url, obj, new Response.Listener<JSONObject>() {
+
+                                                @Override
+                                                public void onResponse(JSONObject response) {
+                                                    System.out.println(response.toString());
+
+                                                }
+                                            }, new Response.ErrorListener() {
+
+                                                @Override
+                                                public void onErrorResponse(VolleyError error) {
+                                                    // TODO Auto-generated method stub
+                                                    //System.out.println(error.networkResponse.data);
+                                                    System.out.println("Error");
+
+                                                }
+                                            }){
+
+                                        @Override
+                                        public Map<String, String> getHeaders() throws AuthFailureError {
+                                            Map<String, String>  params = new HashMap<String, String>();
+                                            params.put("accept", "application/json");
+                                            params.put("Content-Type", "application/json");
+                                            params.put("accessToken", "Bearer " + accessToken);
+                                            System.out.println("Bearer " + accessToken);
+                                            params.put("Accept-Encoding", "gzip");
+                                            params.put("requestId", requestID);
+                                            System.out.println(requestID);
+                                            params.put("thirdPartyAccessId", accessID);
+                                            System.out.println(accessID);
+                                            params.put("apiRegistrationId", registration);
+                                            System.out.println(registration);
+                                            params.put("deviceId", Build.SERIAL);
+                                            System.out.println(Build.SERIAL);
+                                            System.out.println(strippedPhone);
+                                            System.out.println(amount);
+
+                                            return params;
+                                        }
+                                    };
+
+                                    mRequestQueue.add(jsObjRequest);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+                            System.out.println(error);
+
+                        }
+                    }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String>  params = new HashMap<String, String>();
+                    params.put("secretKey", secretKey);
+                    params.put("salt", salt);
+                    params.put("thirdPartyAccessId", accessID);
+
+                    return params;
+                }
+            };
+            mRequestQueue.add(jsObjRequest);
+
+
 
         } else
             mTextView.setText("Waiting for NDEF Message");
