dfdca34adec2f74ebb72d4db3cad818d41f9e517
Kevin Cho <kevc.pro@gmail.com>
2018-01-21 09:54:23 -0500

    xDfrontend

--- app/src/main/java/com/intertapp/NFCActivity.java
+++ app/src/main/java/com/intertapp/NFCActivity.java
@@ -8,8 +8,7 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,31 +16,10 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Cache;
-import com.android.volley.Network;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
 
-    private EditText mEditText, amount;
+    private EditText amount;
     private final String accessID = "CA1TAaPamkkrCsZQ";
     private final String registration = "CA1AReggCJXVeGF2";
     private final String secretKey = "XyhKlB3ySVGpeNHAWR7XwkASvpiymC/Sr4GQDyZs+YM="; //lul
@@ -49,23 +27,29 @@ public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessag
     private String accessToken;
     private Button getStarted;
     private ImageButton backbutton;
-    private TextView instr;
+    private TextView mTextView, instr, xD, sendm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
-        mEditText = (EditText) findViewById(R.id.edit_text_field);
+        mTextView = (TextView) findViewById(R.id.textView2);
         amount = (EditText) findViewById(R.id.edit_text_field);
-        instr = (TextView) findViewById(R.id.editText2);
+        instr = (TextView) findViewById(R.id.textView);
+        xD= (TextView) findViewById(R.id.textView3);
+        sendm = (TextView)  findViewById(R.id.editText3);
+        //String str = "Send money in an instant while";
+        //mTextView.setText((str));
         Typeface face = Typeface.createFromAsset(getAssets(),
                 "fonts/futur.ttf");
-        mEditText.setTypeface(face);
+        mTextView.setTypeface(face);
         amount.setTypeface(face);
+        xD.setTypeface(face);
         instr.setTypeface(face);
+        sendm.setTypeface(face);
         NfcAdapter mAdapter = NfcAdapter.getDefaultAdapter(this);
         if (mAdapter == null) {
-            mEditText.setText("Sorry this device does not have NFC.");
+            mTextView.setText("Sorry this device does not have NFC.");
             return;
         }
 
@@ -87,7 +71,8 @@ public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessag
             @Override
             public void onClick(View view) {
 
-
+            mTextView.setVisibility(View.INVISIBLE);
+            xD.setVisibility(View.INVISIBLE);
             amount.setVisibility(View.VISIBLE);
             instr.setVisibility(View.INVISIBLE);
             getStarted.setVisibility(View.INVISIBLE);
@@ -103,7 +88,7 @@ public class NFCActivity extends Activity implements NfcAdapter.CreateNdefMessag
     @Override
     public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
 
-        String request = mEditText.getText().toString();
+        String request = amount.getText().toString();
         NdefRecord ndefRecord = NdefRecord.createMime("text/plain", request.getBytes());
         NdefMessage ndefMessage = new NdefMessage(ndefRecord);
         return ndefMessage;
